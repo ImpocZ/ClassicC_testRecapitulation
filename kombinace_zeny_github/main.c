c9dd7c3b867b13079fdf49161cccedaeb4eebb10
ImpocZ <knightradim.kall@gmail.com>
2025-10-01 17:49:50 +0200

    'kombinace' finished, 'slalom' next.

--- kombinace_zeny_github/main.c
+++ kombinace_zeny_github/main.c
@@ -114,7 +114,7 @@ FILE *openFile(int foMode, const char *file, FILE *fName){
             printf("File %s closed successfully.\n", file);
         }
         else {
-            printf("Error encountere while tring to close the file %s!\n", file);
+            printf("Error encountered while tring to close the file %s!\n", file);
             exit(file == INPUT ? -101 : - 91);
         }
         return fName;    
@@ -125,23 +125,79 @@ FILE *openFile(int foMode, const char *file, FILE *fName){
 
 void printOut(DATA *data, int total){
     printf("Alpske lyzovani startovni listina K O M B I N A C E  Z E N Y\n"
-        "--------------------------------------------------------------------------------\n"
-        "startovni cislo    jmeno       prijmeni  stat      sjezd  slalom\n"
-        "--------------------------------------------------------------------------------\n"
+        "------------------------------------------------------------------------------------\n"
+        "startovni cislo                 jmeno               prijmeni  stat   sjezd  slalom\n"
+        "------------------------------------------------------------------------------------\n"
     );
     for (int i = 0; i < total; i++)
     {
-        printf("        %02d  %20s   %20s   %s  %d:%02d,%02d  %2d,%02d\n", data[i].position, data[i].firstname,
+        printf("             %02d  %20s   %20s   %s  %d:%02d,%02d  %2d,%02d\n", data[i].position, data[i].firstname,
             data[i].surname, data[i].nation, data[i].firstTime.min, data[i].firstTime.sec, data[i].firstTime.centisec,
-            data[i].secondTime.seconds, data[i].secondTime.seconds);
+            data[i].secondTime.seconds, data[i].secondTime.centisec);
+    }
+}
+
+int convertTime(FIRST_TIME ft, SECOND_TIME st){
+    // min to sec is "min * 60", sec to centisec is "sec * 100". As such mins to centisec is "min * 6000"
+    return ft.min * 6000 + (ft.sec + st.seconds) * 100 + (ft.centisec + st.centisec); 
+}
+
+void bubbleSort(DATA *data, int total){
+    for (int i = 0; i < total - 1; i++)
+    {
+        int swapped = 0;
+        for (int j = 0; j < total - 1 - i; j++)
+        {
+            if (convertTime(data[j].firstTime, data[j].secondTime) > convertTime(data[j + 1].firstTime, data[j + 1].secondTime))
+            {
+                DATA temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+                swapped++;
+            }            
+        }
+        if (swapped == 0){
+            break;
+        }
+    }
+}
+
+void *formattedTime(int totalTime, char *formStr){
+    int min = totalTime / 6000;
+    int sec = (totalTime % 6000) / 100;
+    int centisec = (totalTime % 6000) % 100;
+
+    sprintf(formStr, "%d:%02d,%02d", min, sec, centisec);
+}
+
+void fprintOut(DATA *data, int total){
+    char finishTimes[10];
+    char lossOnVTimes[10];
+    FILE *fw = openFile(0, OUTPUT, fw);
+
+    fprintf(fw, "A L P S K E  L Y Z O V A N I - K O M B I N A C E  Z E N Y\n"
+        "--------------------------------------------------------------------------------------------------------------\n"
+        "poradi              jmeno            prijmeni stat   sjezd   slalom   celkem    ztrata\n"
+        "--------------------------------------------------------------------------------------------------------------\n");
+    
+    for (int i = 0; i < total; i++)
+    {
+        formattedTime(convertTime(data[i].firstTime, data[i].secondTime), finishTimes);
+        formattedTime(convertTime(data[i].firstTime, data[i].secondTime) - convertTime(data[0].firstTime, data[0].secondTime), lossOnVTimes);
+        fprintf(fw, "  %02d.%20s%20s  %s  %d:%02d,%02d  %2d,%02d   %s   %s\n", i + 1, data[i].firstname, data[i].surname, data[i].nation,
+            data[i].firstTime.min, data[i].firstTime.sec, data[i].firstTime.centisec, data[i].secondTime.seconds, data[i].secondTime.centisec, finishTimes,
+            i == 0 ? "-------" : lossOnVTimes);
     }
     
+    fw = openFile(1, OUTPUT, fw);
 }
 
 int main(){
     int total = 0;
     DATA *data = fillData(&total);
     printOut(data, total);
+    bubbleSort(data, total);
+    fprintOut(data, total);
 
     free(data);
     printf("All done..\n");
